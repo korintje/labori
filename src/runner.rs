fa6f6ffbe0ddf11daafe7efc3910bb69bae0c923
korintje <t.hosomi1410@gmail.com>
2022-06-27 04:03:40 +0000

    wip

--- src/runner.rs
+++ src/runner.rs
@@ -8,57 +8,62 @@ use encoding::{Encoding, EncoderTrap};
 use encoding::all::ASCII;
 use tokio::time::{sleep, Duration};
 
-pub async fn run(config: Config, tx0: mpsc::Sender<Vec<u8>>, rx1: mpsc::Receiver<Signal>) -> Result<(), LaboriError> {
+pub async fn run(config: Config, tx0: mpsc::Sender<Vec<u8>>, mut rx1: mpsc::Receiver<Signal>) 
+-> Result<(), LaboriError> {
 
     // Prepare command bytes
-    let trigger_cmd = ":LOG:LEN 5e5; :LOG:CLE; :FUNC FINA; :GATE:TIME 0.1; :FRUN ON\n";
+    // let trigger_cmd = ":LOG:LEN 5e5; :LOG:CLE; :FUNC FINA; :GATE:TIME 0.1; :FRUN ON\n";
+    let trigger_cmd = ":LOG:LEN 5e5; :LOG:CLE; :FRUN ON\n";
     let trigger_cmd = ASCII.encode(trigger_cmd, EncoderTrap::Replace).unwrap();
     let polling_cmd = ":LOG:DATA?\n";
     let polling_cmd = ASCII.encode(polling_cmd, EncoderTrap::Replace).unwrap();
 
-    let addr = "192.168.200.44:5198";
-    if let Ok(stream) = TcpStream::connect(addr) {
+    loop {
 
-        println!("Connection Ok.");
+        let signal = if let Some(item) = rx1.recv().await {item} else {continue};
+        if let Signal::Start = signal {
 
-        // Prepare buffers
-        let mut reader = BufReader::new(&stream);
-        let mut writer = BufWriter::new(&stream);
+            if let Ok(stream) = TcpStream::connect(&config.device_addr) {
 
-        // Trigger measurements
-        writer.write(&trigger_cmd).expect("Trigger FAILURE!!!");
-        writer.flush().unwrap();
-        sleep(Duration::from_millis(1000)).await;
+                println!("Connection Ok.");
+    
+                // Prepare buffers
+                let mut reader = BufReader::new(&stream);
+                let mut writer = BufWriter::new(&stream);
+    
+                // Trigger measurements
+                writer.write(&trigger_cmd).expect("Trigger FAILURE!!!");
+                writer.flush().unwrap();
+                sleep(Duration::from_millis(1000)).await;            
 
-        // Data polling loop
-        loop {
+                // Data polling loop
+                loop {
 
-            writer.write(&polling_cmd).expect("Polling FAILURE!!!");
-            writer.flush().unwrap();
+                    writer.write(&polling_cmd).expect("Polling FAILURE!!!");
+                    writer.flush().unwrap();
 
-            let mut buff = vec![0; 1024];
-            let n = reader.read(&mut buff).expect("RECEIVE FAILURE!!!");
-            // println!("{:?}", &buff[..n]);
-            println!("{}", n);
-            // println!("{:?}", &buff[0..n]);
-            
-            if n >= 2 {
-                // println!("{:?}", &buff[..n]);
-                if let Err(e) = tx0.send(buff[..n].to_vec()).await {
-                    panic!("Failed to send {}", e)
-                };
-                
+                    let mut buff = vec![0; 1024];
+                    let n = reader.read(&mut buff).expect("RECEIVE FAILURE!!!");
+                    println!("{}", n);
+                    
+                    if n >= 2 {
+                        if let Err(e) = tx0.send(buff[..n].to_vec()).await {
+                            println!("Failed to send {}", e)
+                        };
+                    }
+                    
+                    // Controll polling interval
+                    sleep(Duration::from_millis(10)).await;
+                    
+                    // Check stop signal
+                    if let Some(signal) = rx1.recv().await {
+                        if let Signal::Stop = signal {
+                            break                        
+                        }
+                    }
+                    
+                }
             }
-            
-            sleep(Duration::from_millis(10000)).await;
-            // println!("100 ms have elapsed");      
         }
-
-    } else {
-        println!("Couldn't connect to server...");
     }
-
-    println!("finished");
-    Ok(())
-
-  }
+}
